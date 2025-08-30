c50d1ab2c563e1607247831815e01d5105939116
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-08-30 11:28:05 +0500

    concept: switch case and more tests

--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,27 @@ package main
 
 import "fmt"
 
-const helloPrefix = "Hello, "
-func Hello(name string) string {
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return helloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+		case spanish:
+			prefix = spanishHelloPrefix
+		case french:
+			prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("Awwab"))
+	fmt.Println(Hello("Awwab", ""))
 }
