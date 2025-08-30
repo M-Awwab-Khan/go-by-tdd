9312cede4fb128e7c6c992a443f8b964f5932cfa
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-08-30 15:33:44 +0500

    concept: variadic functions, function variable, slicing slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,35 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(slices ...[]int) []int {
+	// lengthOfSlices := len(slices)
+	// sums := make([]int, lengthOfSlices)
+
+	// for i, slice := range slices {
+	// 	sums[i] = Sum(slice)
+	// }
+	// return sums;
+
+	var sums []int
+
+	for _, slice := range slices {
+		sums = append(sums, Sum(slice));
+	}
+	return sums;
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+
+	for _, slice := range slices {
+		if len(slice) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := slice[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
