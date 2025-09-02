aadb2fca645e47d47662b37f6a7784471df60a02
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2025-09-02 11:04:23 +0500

    lesson 6: pointers and errors

---
+++ pointers/wallet.go
@@ -0,0 +1,33 @@
+package pointers
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+type Bitcoin int
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Deposit(amt Bitcoin) {
+	w.balance += amt
+}
+
+func (w *Wallet) Withdraw(amt Bitcoin) error {
+	if amt > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= amt
+	return nil
+}
--- struct-methods-interfaces/shapes_test.go
+++ struct-methods-interfaces/shapes_test.go
@@ -3,12 +3,12 @@ package shapes
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0};
-	got := rectangle.Perimeter();
-	want := 40.0;
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
 
 	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want);
+		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
 
@@ -35,14 +35,18 @@ func TestPerimeter(t *testing.T) {
 // }
 
 func TestArea(t *testing.T) {
-	areaTests := []struct {shape Shape; want float64; name string} {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+		name  string
+	}{
 		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-        {name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
-        {name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{name: "Circle", shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
 	for _, tt := range areaTests {
-			t.Run(tt.name, func(t *testing.T) {
+		t.Run(tt.name, func(t *testing.T) {
 
 			got := tt.shape.Area()
 			want := tt.want
@@ -50,6 +54,6 @@ func TestArea(t *testing.T) {
 			if got != want {
 				t.Errorf("%#v got %g want %g", tt.shape, got, tt.want)
 			}
-	})
-}
+		})
+	}
 }
